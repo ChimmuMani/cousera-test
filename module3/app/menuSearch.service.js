e147e515a0296b50257f0482df4e0c01e0e7ed68
chimz <34434560+ChimmuMani@users.noreply.github.com>
2020-10-18 07:25:37 -0400

    assignment3

--- module3/app/menuSearch.service.js
+++ module3/app/menuSearch.service.js
@@ -8,6 +8,24 @@
       getMatchedMenuItems:getMatchedMenuItems
     };
     return service;
+    function getMatchedMenuItems(SearchTerm){
+      if(SearchTerm==''){
+        return $q.when([]);
+      }
+      return $http.get(ApiBasePath+"/menu_items.json")
+      .then(getMenuItemsComplete);// response contains data
+      function getMenuItemsComplete(data){  //  console.log(data.menu_items);
+        var foundItems=data.menu_items;//menu_items look on json
+        foundItems=foundItems.filter(function(item){
+          return item.description.indexOf(SearchTerm)!=-1;
+        });
+         return foundItems || [];
+      }
+
+    }
+    }
+    })();
+
 
 /* difference between factor and service // Servicefunction InboxService($http) {
   this.getEmails = function getEmails() {
@@ -30,24 +48,3 @@ angular
   .module('app')
   .factory('InboxService', InboxService);
 }*/
-
-function getMatchedMenuItems(SearchTerm){
-  if(SearchTerm==''){
-    return $q.when([]);
-  }
-  return $http.get(ApiBasePath+"/menu_items.json")
-  .then(getMenuItemsComplete);// response contains data
-
-  function getMenuItemsComplete(data){
-  //  console.log(data.menu_items);
-    var foundItems=data.menu_items;//menu_items look on json
-  
-    foundItems=foundItems.filter(function(item){
-      return item.description.indexOf(SearchTerm)!=-1;
-    });
-     return foundItems || [];
-  }
-
-}
-}
-})();
