e8d47310b200238c7e4e2a122d5fed259cfebed3
chimz <34434560+ChimmuMani@users.noreply.github.com>
2020-10-18 08:27:51 -0400

    assignment3

--- module3/app/menuSearch.service.js
+++ module3/app/menuSearch.service.js
@@ -1,31 +1,42 @@
-(function(){
+(function() {
   'use strict';
-    angular.module('myApp')
-  .factory('MenuSearchService',MenuSearchService);
+
+  angular.module('NarrowItDownApp')
+    .factory('MenuSearchService', MenuSearchService);
+
   MenuSearchService.$inject = ['$http', '$q', 'ApiBasePath']
-  function MenuSearchService($http,$q,ApiBasePath){
-    var service={
-      getMatchedMenuItems:getMatchedMenuItems
+  function MenuSearchService($http, $q, ApiBasePath) {
+    var service = {
+      getMatchedMenuItems: getMatchedMenuItems
     };
+
     return service;
-    function getMatchedMenuItems(searchTerm){
-      if(searchTerm==''){
-        return $q.when([]);
+
+    ////////////
+
+    function getMatchedMenuItems(searchTerm) {
+
+      if (searchTerm == '') {
+       return $q.when([]);
       }
-      return $http.get(ApiBasePath+"/menu_items.json")
-      .then(getMenuItemsComplete);// response contains data
+
+      return $http.get(ApiBasePath + "/menu_items.json")
+        .then(getMenuItemsComplete);
+
       function getMenuItemsComplete(data) {
-          var foundItems = data.menu_items;
+        var foundItems = data.menu_items;
 
-          foundItems = foundItems.filter(function(item) {
-            return item.description.indexOf(searchTerm) !== -1;
-          });
-          return foundItems || [];
-        }
-    }
-    }
-    })();
+        foundItems = foundItems.filter(function(item) {
+          return item.description.indexOf(searchTerm) !== -1;
+        });
+        return foundItems || [];
+      }
 
+    }
+
+  }
+
+})();
 
 /* difference between factor and service // Servicefunction InboxService($http) {
   this.getEmails = function getEmails() {
