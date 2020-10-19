b6eed1ceef566e5838f671d0e2fc9e6867424e0c
chimz <34434560+ChimmuMani@users.noreply.github.com>
2020-10-19 13:41:53 -0400

    amw1

--- module3/app/foundItems/found-items.directive.js
+++ module3/app/foundItems/found-items.directive.js
@@ -2,11 +2,10 @@
   'use strict';
 angular.module('myApp')
   .directive('foundItems',foundItems);
-    console.log("helloooo freom foundItemsout");
   function foundItems(){
   console.log("helloooo freom foundItems");
     var ddo={
-      templateUrl:'app/found-items/found-items.html',
+      templateUrl:'app/foundItems/found-items.html',
       scope:{
         items:'<',
         message:'<',
