ba300c978c3e78aa466730afc5608a8175225f18
chimz <34434560+ChimmuMani@users.noreply.github.com>
2020-10-19 07:07:30 -0400

    a

--- module3/app/loader/loader.directive.js
+++ module3/app/loader/loader.directive.js
@@ -2,6 +2,7 @@
   'use strict',
   angular.module('myApp')
   .directive('loader',loader);
+  cosole.log("Loader  says hello");
   function loader(){
     var ddo={
       templateUrl:"app/loader/loader.template.html"
