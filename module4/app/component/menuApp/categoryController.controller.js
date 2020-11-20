a84e733ffcb96dd167fb456230a7c93823a22382
chimz <34434560+ChimmuMani@users.noreply.github.com>
2020-11-20 15:26:43 -0500

    assignment4

--- module4/app/component/menuApp/categoryController.controller.js
+++ module4/app/component/menuApp/categoryController.controller.js
@@ -8,6 +8,7 @@
 
  categoryController.$inject = ['categoriesList']
   function  categoryController(categoriesList) {
+    console.log("I am in Category Controller");
     var categories = this;
     categories.pageTitle = 'Category';
     categories.categoriesList = categoriesList;
