aeb3469d256dee3c97dd019fbdc949917feea08a
chimz <34434560+ChimmuMani@users.noreply.github.com>
2020-11-20 14:13:04 -0500

    assignment4

--- module4/app/constant/constant.js
+++ module4/app/constant/constant.js
@@ -1,5 +1,5 @@
 (function() {
   'use strict';
   angular.module('Data')
-    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+    .constant('ApiBasePath', "//davids-restaurant.herokuapp.com");
 })();
