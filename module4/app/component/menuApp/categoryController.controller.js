ce789b08854e9cdd758b5be83bfc29606c48f378
chimz <34434560+ChimmuMani@users.noreply.github.com>
2020-11-21 09:00:53 -0500

    assignment4.512

--- module4/app/component/menuApp/categoryController.controller.js
+++ module4/app/component/menuApp/categoryController.controller.js
@@ -2,10 +2,10 @@
 (function () {
   'use strict';
   angular
-    .module('Data')
-    .controller('CategoriesCtrl', CategoriesCtrl);
- CategoriesCtrl.$inject = ['categoriesList']
-  function  CategoriesCtrl(categoriesList) {
+  .module('Data')
+  .controller('CategoriesCtrl', CategoriesCtrl);
+  CategoriesCtrl.$inject = ['cateList']
+  function  CategoriesCtrl(cateList) {
     console.log("I am in Category Controller");
     var categories = this;
     categories.pageTitle = 'Category';
